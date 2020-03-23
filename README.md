753e281a065aed557a30383886b3eccb43fb3476
Rune Nielsen <runeanielsen@live.dk>
2020-03-23 20:04:13 +0100

    adds gdb console app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OPEN-FTTH Geodatabase integrator
+
---
+++ src/OpenFTTH.GDBIntegrator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OpenFTTH.GDBIntegrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
