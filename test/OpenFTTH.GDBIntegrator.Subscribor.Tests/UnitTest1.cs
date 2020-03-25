db31bb3ac21066dde8387c6479a7f69dce3c694d
Rune Nielsen <runeanielsen@live.dk>
2020-03-25 09:23:46 +0100

    adds subscribor library

---
+++ test/OpenFTTH.GDBIntegrator.Subscribor.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace OpenFTTH.GDBIntegrator.Subscribor.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
