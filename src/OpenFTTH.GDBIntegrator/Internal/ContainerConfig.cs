66f41e1a0782f492663492d55ddfac90c07dbcc8
Rune Nielsen <runeanielsen@live.dk>
2020-03-26 09:42:15 +0100

    adds postgres subscriber to dependency

--- src/OpenFTTH.GDBIntegrator/Internal/ContainerConfig.cs
+++ src/OpenFTTH.GDBIntegrator/Internal/ContainerConfig.cs
@@ -3,6 +3,8 @@ using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using OpenFTTH.GDBIntegrator.Subscriber.Kafka;
+using OpenFTTH.GDBIntegrator.Subscriber;
 
 namespace OpenFTTH.GDBIntegrator.Internal
 {
@@ -10,14 +12,10 @@ namespace OpenFTTH.GDBIntegrator.Internal
     {
         public static IServiceProvider Configure()
         {
-            var serviceCollection = new ServiceCollection()
-                .AddLogging()
-                .Configure<LoggerFilterOptions>(x => x.MinLevel = LogLevel.Trace);
-
+            var serviceCollection = SetupServiceCollection();
             var containerBuilder = new ContainerBuilder();
 
             containerBuilder.Populate(serviceCollection);
-
             RegisterTypes(containerBuilder);
 
             var container = containerBuilder.Build();
@@ -25,9 +23,17 @@ namespace OpenFTTH.GDBIntegrator.Internal
             return new AutofacServiceProvider(container);
         }
 
+        private static IServiceCollection SetupServiceCollection()
+        {
+            return new ServiceCollection()
+                .AddLogging()
+                .Configure<LoggerFilterOptions>(x => x.MinLevel = LogLevel.Trace);
+        }
+
         private static void RegisterTypes(ContainerBuilder containerBuilder)
         {
             containerBuilder.RegisterType<Startup>().As<IStartup>().OwnedByLifetimeScope();
+            containerBuilder.RegisterType<PostgresSubscriber>().As<ISubscriber>().OwnedByLifetimeScope();
         }
     }
 }
--- src/OpenFTTH.GDBIntegrator/Startup.cs
+++ src/OpenFTTH.GDBIntegrator/Startup.cs
@@ -1,9 +1,21 @@
+using OpenFTTH.GDBIntegrator.Subscriber;
+
 namespace OpenFTTH.GDBIntegrator
 {
     public class Startup : IStartup
     {
+        private ISubscriber _subscriber;
+
+        public Startup(ISubscriber subscriber)
+        {
+            _subscriber = subscriber;
+        }
+
         public void Start()
         {
+            _subscriber.Subscribe();
+
+            System.Console.ReadLine();
         }
     }
 }
