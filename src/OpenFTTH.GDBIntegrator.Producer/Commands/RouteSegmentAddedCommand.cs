2cbcf4cf82d6e78980ae90d1a4767dc5f580399e
Rune Nielsen <runeanielsen@live.dk>
2020-07-03 11:07:58 +0200

    updates routenetwork events

--- src/OpenFTTH.GDBIntegrator.Producer/Commands/RouteSegmentAddedCommand.cs
+++ src/OpenFTTH.GDBIntegrator.Producer/Commands/RouteSegmentAddedCommand.cs
@@ -6,26 +6,28 @@ using Topos.Producer;
 
 namespace OpenFTTH.GDBIntegrator.Producer.Commands
 {
-    public class RouteNodeAddedCommand : IRequest
+    public class RouteSegmentAddedCommand : IRequest
     {
-        public string EventType => "RouteNodeAddedCommand";
+        public string EventType => "RouteSegmentAddedCommand";
         public string EventId { get; set; }
         public string EventTs => DateTime.UtcNow.ToString();
         public string CmdId => Guid.NewGuid().ToString();
-        public string NodeId { get; set; }
+        public string SegmentId { get; set; }
+        public string FromNodeId { get; set; }
+        public string ToNodeId { get; set; }
         public string Geometry { get; set; }
     }
 
-    public class RouteNodeAddedCommandHandler : IRequestHandler<RouteNodeAddedCommand>
+    public class RouteSegmentAddedCommandHandler : IRequestHandler<RouteSegmentAddedCommand>
     {
         private readonly IProducer _producer;
 
-        public RouteNodeAddedCommandHandler(IProducer producer)
+        public RouteSegmentAddedCommandHandler(IProducer producer)
         {
             _producer = producer;
         }
 
-        public async Task<Unit> Handle(RouteNodeAddedCommand request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(RouteSegmentAddedCommand request, CancellationToken cancellationToken)
         {
             await _producer.Produce("event.route-network", new ToposMessage(request));
             return default;
