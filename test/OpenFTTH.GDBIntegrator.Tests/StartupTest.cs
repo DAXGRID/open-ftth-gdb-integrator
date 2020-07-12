293b52d2abfc1ff9a04887cf0e322bdfe09ac7e8
Rune Nielsen <runeanielsen@live.dk>
2020-07-12 20:14:30 +0200

    adds tests for startup

---
+++ test/OpenFTTH.GDBIntegrator.Tests/StartupTest.cs
@@ -0,0 +1,48 @@
+using OpenFTTH.GDBIntegrator;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FakeItEasy;
+using Xunit;
+using OpenFTTH.GDBIntegrator.Subscriber;
+using OpenFTTH.GDBIntegrator.Producer;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+
+namespace OpenFTTH.GDBIntegrator.Tests
+{
+    public class StartupTest
+    {
+        [Fact]
+        public async Task StartAsync_ShouldNotThrow_OnBeingCalled()
+        {
+            var routeSegmentSubscriber = A.Fake<IRouteSegmentSubscriber>();
+            var producer = A.Fake<IProducer>();
+            var logger = A.Fake<ILogger<Startup>>();
+            var hostApplicationLifetime = A.Fake<IHostApplicationLifetime>();
+
+            var startup = new Startup(routeSegmentSubscriber, producer, logger, hostApplicationLifetime);
+
+            Func<Task> act = async () => { await startup.StartAsync(new CancellationToken()); };
+
+            await act.Should().NotThrowAsync();
+        }
+
+        [Fact]
+        public async Task StopAsync_ShouldNotThrow_OnBeingCalled()
+        {
+            var routeSegmentSubscriber = A.Fake<IRouteSegmentSubscriber>();
+            var producer = A.Fake<IProducer>();
+            var logger = A.Fake<ILogger<Startup>>();
+            var hostApplicationLifetime = A.Fake<IHostApplicationLifetime>();
+
+            var startup = new Startup(routeSegmentSubscriber, producer, logger, hostApplicationLifetime);
+
+            Func<Task> act = async () => { await startup.StopAsync(new CancellationToken()); };
+
+            await act.Should().NotThrowAsync();
+        }
+    }
+}
