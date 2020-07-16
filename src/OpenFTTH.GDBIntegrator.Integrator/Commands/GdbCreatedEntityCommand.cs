03f5c1f173c791b86cb0d09437f6103524874205
Rune Nielsen <runeanielsen@live.dk>
2020-07-16 15:22:59 +0200

    updates logging

--- src/OpenFTTH.GDBIntegrator.Integrator/Commands/GdbCreatedEntityCommand.cs
+++ src/OpenFTTH.GDBIntegrator.Integrator/Commands/GdbCreatedEntityCommand.cs
@@ -2,7 +2,9 @@ using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
+using OpenFTTH.GDBIntegrator.Config;
 
 namespace OpenFTTH.GDBIntegrator.Integrator.Commands
 {
@@ -13,16 +15,18 @@ namespace OpenFTTH.GDBIntegrator.Integrator.Commands
     public class GdbCreatedEntityCommandHandler : IRequestHandler<GdbCreatedEntityCommand, Unit>
     {
         private readonly ILogger<GdbCreatedEntityCommandHandler> _logger;
+        private readonly ApplicationSetting _applicationSetting;
 
-        public GdbCreatedEntityCommandHandler(ILogger<GdbCreatedEntityCommandHandler> logger)
+        public GdbCreatedEntityCommandHandler(ILogger<GdbCreatedEntityCommandHandler> logger, IOptions<ApplicationSetting> applicationSetting)
         {
             _logger = logger;
+            _applicationSetting = applicationSetting.Value;
         }
 
         public async Task<Unit> Handle(GdbCreatedEntityCommand command, CancellationToken token)
         {
-            _logger.LogInformation($"{DateTime.UtcNow.ToString("o")}: Received message with entity"
-                                   + "that was crated by GdbIntegrator therefore no action is taken");
+            _logger.LogInformation(
+                $"{DateTime.UtcNow.ToString("o")}: no action taken. Entity was created by {_applicationSetting.ApplicationName}");
 
             return await Task.FromResult(new Unit());
         }
--- test/OpenFTTH.GDBIntegrator.Integrator.Tests/Commands/GdbCreatedEntityCommandHandlerTest.cs
+++ test/OpenFTTH.GDBIntegrator.Integrator.Tests/Commands/GdbCreatedEntityCommandHandlerTest.cs
@@ -3,7 +3,9 @@ using FakeItEasy;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenFTTH.GDBIntegrator.Integrator.Commands;
+using OpenFTTH.GDBIntegrator.Config;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using FluentAssertions;
 
 namespace OpenFTTH.GDBIntegrator.Integrator.Tests.Commands
@@ -14,8 +16,9 @@ namespace OpenFTTH.GDBIntegrator.Integrator.Tests.Commands
         public async Task Handle_ShouldJustExecute_OnBeingCalled()
         {
             var logger = A.Fake<ILogger<GdbCreatedEntityCommandHandler>>();
+            var applicationSetting = A.Fake<IOptions<ApplicationSetting>>();
 
-            var gdbCommandHandler = new GdbCreatedEntityCommandHandler(logger);
+            var gdbCommandHandler = new GdbCreatedEntityCommandHandler(logger, applicationSetting);
 
             var result = await gdbCommandHandler.Handle(new GdbCreatedEntityCommand(), new CancellationToken());
 
