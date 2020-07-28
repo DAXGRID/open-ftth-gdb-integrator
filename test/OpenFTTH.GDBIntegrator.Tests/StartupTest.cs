bf7f7bd0cf288e31a56af00d83c483155f9f7449
Rune Nielsen <runeanielsen@live.dk>
2020-07-28 15:00:16 +0200

    fixes failing tests

--- test/OpenFTTH.GDBIntegrator.Tests/StartupTest.cs
+++ test/OpenFTTH.GDBIntegrator.Tests/StartupTest.cs
@@ -9,6 +9,7 @@ using OpenFTTH.GDBIntegrator.Subscriber;
 using OpenFTTH.GDBIntegrator.Producer;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Hosting;
+using FluentMigrator.Runner;
 
 namespace OpenFTTH.GDBIntegrator.Tests
 {
@@ -22,8 +23,9 @@ namespace OpenFTTH.GDBIntegrator.Tests
             var producer = A.Fake<IProducer>();
             var logger = A.Fake<ILogger<Startup>>();
             var hostApplicationLifetime = A.Fake<IHostApplicationLifetime>();
+            var migrator = A.Fake<IMigrationRunner>();
 
-            var startup = new Startup(routeSegmentSubscriber, routeNodeSubscriber, producer, logger, hostApplicationLifetime);
+            var startup = new Startup(routeSegmentSubscriber, routeNodeSubscriber, producer, logger, hostApplicationLifetime, migrator);
 
             Func<Task> act = async () => { await startup.StartAsync(new CancellationToken()); };
 
@@ -38,8 +40,9 @@ namespace OpenFTTH.GDBIntegrator.Tests
             var producer = A.Fake<IProducer>();
             var logger = A.Fake<ILogger<Startup>>();
             var hostApplicationLifetime = A.Fake<IHostApplicationLifetime>();
+            var migrator = A.Fake<IMigrationRunner>();
 
-            var startup = new Startup(routeSegmentSubscriber, routeNodeSubscriber, producer, logger, hostApplicationLifetime);
+            var startup = new Startup(routeSegmentSubscriber, routeNodeSubscriber, producer, logger, hostApplicationLifetime, migrator);
 
             Func<Task> act = async () => { await startup.StopAsync(new CancellationToken()); };
 
