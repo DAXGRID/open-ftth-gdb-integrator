5917a05319e71a335fe38b58fd12fcba2d6c216b
Rune Nielsen <runeanielsen@live.dk>
2020-07-29 14:26:09 +0200

    fixes typing issues with variables

--- scripts/set-environment-minikube.fish
+++ scripts/set-environment-minikube.fish
@@ -3,24 +3,24 @@
 # Environment variables for fish shell
 
 # Application
-export APPLICATION__NAME=GDB_INTEGRATOR
-export APPLICATION__TOLERANCE=0.01
+export APPLICATION__APPLICATIONNAME="GDB_INTEGRATOR"
+export APPLICATION__TOLERANCE="0.1"
 
 # Postgres
 export POSTGIS__HOST=(minikube ip)
 export POSTGIS__PORT=(kubectl describe service openftth-postgis -n openftth | grep NodePort | grep -o '[0-9]\+')
-export POSTGIS__DATABASE=OPEN_FTTH
-export POSTGIS__USERNAME=postgres
-export POSTGIS__PASSWORD=postgres
+export POSTGIS__DATABASE="OPEN_FTTH"
+export POSTGIS__USERNAME="postgres"
+export POSTGIS__PASSWORD="postgres"
 
 # Kafka
 export KAFKA__SERVER=(minikube ip):(kubectl describe service openftth-kafka-cluster-kafka-external-bootstrap -n openftth | grep NodePort | grep -o '[0-9]\+')
-export KAFKA__POSTGRESROUTESEGMENTTOPIC=postgres.route_network.route_segment
-export KAFKA__POSTGRESROUTENODETOPIC=postgres.route_network.route_node
-export KAFKA__POSTGRESROUTESEGMENTCONSUMER=postgres-routesegment-consumer
-export KAFKA__POSTGRESROUTENODECONSUMER=postgres-routenode-consumer
-export KAFKA__POSITIONFILEPATH=/tmp/
-export KAFKA__EVENTROUTENETWORKTOPICNAME=event.route-network
+export KAFKA__POSTGRESROUTESEGMENTTOPIC="postgres.route_network.route_segment"
+export KAFKA__POSTGRESROUTENODETOPIC="postgres.route_network.route_node"
+export KAFKA__POSTGRESROUTESEGMENTCONSUMER="postgres-routesegment-consumer"
+export KAFKA__POSTGRESROUTENODECONSUMER="postgres-routenode-consumer"
+export KAFKA__POSITIONFILEPATH="/tmp/"
+export KAFKA__EVENTROUTENETWORKTOPICNAME="event.route-network"
 
 # Logging
-export LOGGING__LOGLEVEL__DEFAULT=Information
+export LOGGING__LOGLEVEL__DEFAULT="Information"
