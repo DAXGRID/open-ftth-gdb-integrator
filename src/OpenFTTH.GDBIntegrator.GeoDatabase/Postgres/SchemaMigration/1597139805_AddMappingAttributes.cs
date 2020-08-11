11df53983c8d8d3d9d6d86a36f3105c466f9b970
Rune Nielsen <runeanielsen@live.dk>
2020-08-11 12:11:10 +0200

    adds mapping attributes schema

---
+++ src/OpenFTTH.GDBIntegrator.GeoDatabase/Postgres/SchemaMigration/1597139805_AddMappingAttributes.cs
@@ -0,0 +1,78 @@
+using FluentMigrator;
+
+namespace OpenFTTH.GDBIntegrator.GeoDatabase.Postgres.SchemaMigration
+{
+    [Migration(1597139805)]
+    public class AddMappingAttributes : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("route_segment").InSchema("route_network")
+                .AddColumn("mapping_method").AsInt32().Nullable()
+                .AddColumn("mapping_vertical_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_horizontal_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_source_info").AsString(int.MaxValue).Nullable()
+                .AddColumn("mapping_survey_date").AsDate().Nullable();
+
+            Alter.Table("route_segment").InSchema("route_network_integrator")
+                .AddColumn("mapping_method").AsInt32().Nullable()
+                .AddColumn("mapping_vertical_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_horizontal_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_source_info").AsString(int.MaxValue).Nullable()
+                .AddColumn("mapping_survey_date").AsDate().Nullable();
+
+            Alter.Table("route_node").InSchema("route_network")
+                .AddColumn("mapping_method").AsInt32().Nullable()
+                .AddColumn("mapping_vertical_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_horizontal_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_source_info").AsString(int.MaxValue).Nullable()
+                .AddColumn("mapping_survey_date").AsDate().Nullable();
+
+            Alter.Table("route_node").InSchema("route_network_integrator")
+                .AddColumn("mapping_method").AsInt32().Nullable()
+                .AddColumn("mapping_vertical_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_horizontal_accuracy").AsString(255).Nullable()
+                .AddColumn("mapping_source_info").AsString(int.MaxValue).Nullable()
+                .AddColumn("mapping_survey_date").AsDate().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete
+                .Column("mapping_method")
+                .Column("mapping_vertical_accuracy")
+                .Column("mapping_horizontal_accuracy")
+                .Column("mapping_source_info")
+                .Column("mapping_survey_date")
+                .FromTable("route_segment")
+                .InSchema("route_network");
+
+            Delete
+                .Column("mapping_method")
+                .Column("mapping_vertical_accuracy")
+                .Column("mapping_horizontal_accuracy")
+                .Column("mapping_source_info")
+                .Column("mapping_survey_date")
+                .FromTable("route_segment")
+                .InSchema("route_network_integrator");
+
+            Delete
+                .Column("mapping_method")
+                .Column("mapping_vertical_accuracy")
+                .Column("mapping_horizontal_accuracy")
+                .Column("mapping_source_info")
+                .Column("mapping_survey_date")
+                .FromTable("route_node")
+                .InSchema("route_network");
+
+            Delete
+                .Column("mapping_method")
+                .Column("mapping_vertical_accuracy")
+                .Column("mapping_horizontal_accuracy")
+                .Column("mapping_source_info")
+                .Column("mapping_survey_date")
+                .FromTable("route_node")
+                .InSchema("route_network_integrator");
+        }
+    }
+}
