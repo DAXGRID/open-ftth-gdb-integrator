a216ebd8be99daf60264fc4e7a3e393642c63818
Rune Nielsen <runeanielsen@live.dk>
2020-08-12 16:00:55 +0200

    removes dead using

--- src/OpenFTTH.GDBIntegrator.Integrator/Notifications/RouteSegmentConnectivityChanged.cs
+++ src/OpenFTTH.GDBIntegrator.Integrator/Notifications/RouteSegmentConnectivityChanged.cs
@@ -7,7 +7,6 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
-using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
