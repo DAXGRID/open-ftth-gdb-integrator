ad4d52cd8f5e8a391e812b4ecd0d4ae37c2c2c44
Rune Nielsen <runeanielsen@live.dk>
2020-08-18 20:49:54 +0200

    updates delete_me and mark_as_deleted migration

--- src/OpenFTTH.GDBIntegrator.GeoDatabase/Postgres/SchemaMigration/1596653853_MarkedToBeDeletedAndDeleteMeNotNullable.cs
+++ src/OpenFTTH.GDBIntegrator.GeoDatabase/Postgres/SchemaMigration/1596653853_MarkedToBeDeletedAndDeleteMeNotNullable.cs
@@ -7,10 +7,10 @@ namespace OpenFTTH.GDBIntegrator.GeoDatabase.Postgres.SchemaMigration
     {
         public override void Up()
         {
-            Alter.Table("route_segment").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable();
-            Alter.Table("route_node").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable();
-            Alter.Table("route_segment").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable();
-            Alter.Table("route_node").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable();
+            Alter.Table("route_segment").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable().Indexed();
+            Alter.Table("route_node").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable().Indexed();
+            Alter.Table("route_segment").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable().Indexed();
+            Alter.Table("route_node").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().NotNullable().Indexed();
 
             Alter.Table("route_segment").InSchema("route_network").AlterColumn("delete_me").AsBoolean().NotNullable();
             Alter.Table("route_node").InSchema("route_network").AlterColumn("delete_me").AsBoolean().NotNullable();
@@ -20,15 +20,15 @@ namespace OpenFTTH.GDBIntegrator.GeoDatabase.Postgres.SchemaMigration
 
         public override void Down()
         {
-            Alter.Table("route_segment").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().Nullable();
-            Alter.Table("route_node").InSchema("route_network").AlterColumn("marked_to_be_deleted").AsBoolean().Nullable();
-            Alter.Table("route_segment").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().Nullable();
-            Alter.Table("route_node").InSchema("route_network_integrator").AlterColumn("marked_to_be_deleted").AsBoolean().Nullable();
+            Delete.Column("marked_to_be_deleted").FromTable("route_segment").InSchema("route_network");
+            Delete.Column("marked_to_be_deleted").FromTable("route_segment").InSchema("route_network_integrator");
+            Delete.Column("marked_to_be_deleted").FromTable("route_node").InSchema("route_network");
+            Delete.Column("marked_to_be_deleted").FromTable("route_node").InSchema("route_network_integrator");
 
-            Alter.Table("route_segment").InSchema("route_network").AlterColumn("delete_me").AsBoolean().Nullable();
-            Alter.Table("route_node").InSchema("route_network").AlterColumn("delete_me").AsBoolean().Nullable();
-            Alter.Table("route_segment").InSchema("route_network_integrator").AlterColumn("delete_me").AsBoolean().Nullable();
-            Alter.Table("route_node").InSchema("route_network_integrator").AlterColumn("delete_me").AsBoolean().Nullable();
+            Delete.Column("delete_me").FromTable("route_segment").InSchema("route_network");
+            Delete.Column("delete_me").FromTable("route_segment").InSchema("route_network_integrator");
+            Delete.Column("delete_me").FromTable("route_node").InSchema("route_network");
+            Delete.Column("delete_me").FromTable("route_node").InSchema("route_network_integrator");
         }
     }
 }
