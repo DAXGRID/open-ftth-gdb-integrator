dcdae7409b2b5896306bdc07d34eeff4bb56b641
Rune Nielsen <runeanielsen@live.dk>
2020-09-03 04:38:17 +0200

    cleanup

--- src/OpenFTTH.GDBIntegrator.Integrator/Queue/SemaphoreQueue.cs
+++ src/OpenFTTH.GDBIntegrator.Integrator/Queue/SemaphoreQueue.cs
@@ -7,32 +7,39 @@ namespace OpenFTTH.GDBIntegrator.Integrator.Queue
     public class SemaphoreQueue
     {
         private SemaphoreSlim semaphore;
-        private ConcurrentQueue<TaskCompletionSource<bool>> queue =
-            new ConcurrentQueue<TaskCompletionSource<bool>>();
+        private ConcurrentQueue<TaskCompletionSource<bool>> queue = new ConcurrentQueue<TaskCompletionSource<bool>>();
+
         public SemaphoreQueue(int initialCount)
         {
             semaphore = new SemaphoreSlim(initialCount);
         }
+
         public SemaphoreQueue(int initialCount, int maxCount)
         {
             semaphore = new SemaphoreSlim(initialCount, maxCount);
         }
+
         public void Wait()
         {
             WaitAsync().Wait();
         }
+
         public Task WaitAsync()
         {
-            var tcs = new TaskCompletionSource<bool>();
-            queue.Enqueue(tcs);
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            queue.Enqueue(taskCompletionSource);
+
             semaphore.WaitAsync().ContinueWith(t =>
             {
                 TaskCompletionSource<bool> popped;
                 if (queue.TryDequeue(out popped))
                     popped.SetResult(true);
             });
-            return tcs.Task;
+
+            return taskCompletionSource.Task;
         }
+
         public void Release()
         {
             semaphore.Release();
