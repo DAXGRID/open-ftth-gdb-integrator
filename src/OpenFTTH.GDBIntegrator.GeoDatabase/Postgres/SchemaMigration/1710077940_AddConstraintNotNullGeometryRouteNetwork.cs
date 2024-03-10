553764762d8ba8deefec77f69af4e05885e06485
Rune Nielsen <runenielsen@runbox.com>
2024-03-10 14:50:48 +0100

    add not null contraint for geometry route_node and route_segment (#117)

---
+++ src/OpenFTTH.GDBIntegrator.GeoDatabase/Postgres/SchemaMigration/1710077940_AddConstraintNotNullGeometryRouteNetwork.cs
@@ -0,0 +1,20 @@
+using FluentMigrator;
+using System.IO;
+
+namespace OpenFTTH.GDBIntegrator.GeoDatabase.Postgres.SchemaMigration
+{
+    [Migration(1710077940)]
+    public class AddConstraintNotNullGeometryRouteNetwork : Migration
+    {
+        public override void Up()
+        {
+            Execute.Script(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location)
+                           + "/Postgres/SchemaMigration/Scripts/set_not_null_contraint_route_network.sql");
+        }
+
+        public override void Down()
+        {
+            // This is a hard change to do and needs to be resolved manually if a down is needed to be done.
+        }
+    }
+}
---
+++ src/OpenFTTH.GDBIntegrator.GeoDatabase/Postgres/SchemaMigration/Scripts/set_not_null_contraint_route_network.sql
@@ -0,0 +1,2 @@
+ALTER TABLE route_network.route_node ALTER COLUMN coord SET NOT NULL;
+ALTER TABLE route_network.route_segment ALTER COLUMN coord SET NOT NULL;
